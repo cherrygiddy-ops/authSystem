4010d7868ecb7a0bdbccac3d00ce9cab869e3196
Morris_Cherry <morrischerryg@gmail.com>
2026-08-06 14:42:08 +0700

    Updated CORS FOR Frontend

--- src/main/java/com/authservice/system/auth/SecurityConfig.java
+++ src/main/java/com/authservice/system/auth/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://portal.loholearning.co.ke/", // production frontend (domain)
+                "https://steak.hotelpos.top", // production frontend (domain)
                 "http://localhost:5173"     // local dev (Vite
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
